aa307e979adb5b64a5527319c009b1ab40f56795
Tobi <tobimensch@gmail.com>
2020-04-30 16:44:02 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import sys, os, shutil
+from distutils.core import setup
+
+setup(name='preloadify',
+      version='1.0',
+      description='Create fat binaries with ease.',
+      author='Tobias Glaesser',
+      url='https://github.com/tobimensch/preloadify/',
+      scripts=['preloadify']
+     )
